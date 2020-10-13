bbb2055404d9b521c221d7d11798e1f4c830ad6a
Edge Pair <edgepair@edge-macbook-3.local>
2020-10-13 15:31:58 -0500

    Updates

---
+++ precache-manifest.804df363abd85290360d9a6792e39019.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1a130823329bd7a10e2eaf9bf17ea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8601b9c7156582c32334",
+    "url": "/static/css/main.cdc584d6.chunk.css"
+  },
+  {
+    "revision": "5dc6a8cfa5a863cc534c",
+    "url": "/static/js/2.b32ad5c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b32ad5c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8601b9c7156582c32334",
+    "url": "/static/js/main.2176b226.chunk.js"
+  },
+  {
+    "revision": "390945640477d8aa4442",
+    "url": "/static/js/runtime-main.84308981.js"
+  },
+  {
+    "revision": "97d66d52b7ba15b5841ccde82c2bd2c8",
+    "url": "/static/media/TBot.97d66d52.png"
+  }
+]);
